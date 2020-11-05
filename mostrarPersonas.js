a23ce75105a3636ceb80b42230850e5b587aa64d
AgustinaCozzo <73614124+AgustinaCozzo@users.noreply.github.com>
2020-11-05 01:45:39 -0300

    Update mostrarPersonas.js

--- mostrarPersonas.js
+++ mostrarPersonas.js
@@ -1,14 +1,14 @@
-// var rp = require('request-promise');
-// var options = {
-//     metod: 'GET',
-//     uri: 'https://reclutamiento-14cf7.firebaseio.com/personas.json',
-//     json: true 
-// };
+ var rp = require('request-promise');
+ var options = {
+     metod: 'GET',
+     uri: 'https://reclutamiento-14cf7.firebaseio.com/personas.json',
+     json: true 
+ };
  
-// rp(options)
-//     .then(function (respuesta) {
-//         console.log(respuesta);
-//     })
-//     .catch(function (err) {
-//         console.log('error: ' + err)
-//     });
+ rp(options)
+     .then(function (rta) {
+        console.log(rta);
+    })
+    .catch(function (error2) {
+        console.log('error: ' + error2)
+    });
